9b031084195e3efa540051ae3b3433df74fe7d7a
Michael Kmoch <dudelmicha@googlemail.com>
2025-06-03 16:14:41 +0200

    Michael Kmoch - unify appex/apex and split main from library part in appex so the library can be built without the main

---
+++ src/apps/appex/appex.cpp
@@ -0,0 +1,16 @@
+/*
+ * appex.cpp
+ *
+ *  Created on: 03.06.2025
+ *      Author: michael Kmoch
+ */
+#include "appexFuncs.h"
+int main(int argc, char **argv) {
+	doSomething();
+	return 0;
+}
+
+
+
+
+
