4d6a5b4bbaf06e8ad36aed2c95592020a4829992
Michael Kmoch <dudelmicha@googlemail.com>
2025-06-09 15:21:00 +0000

    WIP: Michael Kmoch - use handles for python integration

--- src/apps/appex/appex.cpp
+++ src/apps/appex/appex.cpp
@@ -104,6 +104,7 @@ int doSomething(void) {
 }
 std::size_t current_key = 0;
 std::map<std::size_t, DgRFNetwork> networks;
+std::map<std::size_t, DgRFNetwork *> networks2;
 std::map<std::size_t, const DgHexIDGGS *> hexes;
 std::map<std::size_t, const DgGeoSphRF *> geoSpheres;
 std::map<std::size_t, const DgIDGG*> dgIDGG;
@@ -125,6 +126,14 @@ std::size_t makeNetwork() {
   networks[key] = DgRFNetwork();
   return key;
 }
+DgRFNetwork* makeNetwork2() {
+  std::size_t key = makeKey();
+  networks2[key] = new DgRFNetwork();
+  cout << "network:" << networks2[key] << endl;
+  return networks2[key];
+}
+void destroyNetwork2(DgRFNetwork* network) {
+}
 void destroyNetwork(std::size_t key) {
   networks.erase(key);
   for (const int &i : hexesOwner[key]) {
@@ -140,6 +149,15 @@ std::size_t makeGeoSphere(std::size_t networkKey, char* type) {
   geoSpheres[key] = DgGeoSphRF::makeRF(networks[networkKey], type);
   geoSpheresOwner[networkKey].push_back(key);
   return key;
+}
+const DgGeoSphRF* makeGeoSphere2(DgRFNetwork* dgRFNetwork, char* type) {
+  //std::size_t key = makeKey();
+  //geoSpheres[key] = DgGeoSphRF::makeRF(*static_cast<DgRFNetwork*>(dgRFNetwork), type);
+  //geoSpheresOwner[networkKey].push_back(key);
+  const DgGeoSphRF* sphere = DgGeoSphRF::makeRF(*dgRFNetwork, type);
+  cout << "network:" << dgRFNetwork << endl;
+  cout << "sphere:" << sphere << endl;
+  return sphere;
 }
 std::size_t makeHex(std::size_t networkKey, std::size_t geoSphereKey,
                     Resoulution resolution, long double lon, long double lat,
@@ -165,17 +183,64 @@ std::size_t makeHex(std::size_t networkKey, std::size_t geoSphereKey,
   hexesOwner[networkKey].push_back(key);
   return key;
 }
+
+const DgHexIDGGS* makeHex2(DgRFNetwork* network, DgGeoSphRF* sphere,
+                    Resoulution resolution, long double lon, long double lat,
+                    bool rads, long double azimuth, int res) {
+  cout << "network:"  << network << endl;
+  cout << "sphere:" << sphere << endl;
+  std::size_t key = makeKey();
+  const DgHexIDGGS* hex = nullptr;
+  switch (resolution) {
+  case ISEA3H:
+    hex =
+        DgIDGGS3H::makeRF(*network, *sphere,
+                          DgGeoCoord(lon, lat, rads), azimuth, res);
+    break;
+  case ISEA4H:
+    hex =
+        DgIDGGS4H::makeRF(*network, *sphere,
+                          DgGeoCoord(lon, lat, rads), azimuth, res);
+    break;
+  case ISEA7H:
+    hex =
+        DgIDGGS7H::makeRF(*network, *sphere,
+                          DgGeoCoord(lon, lat, rads), azimuth, res);
+    break;
+  }
+  cout << "hex:" << hex << endl;
+  hexes[key] = hex;
+  return hex;
+}
 std::size_t makeIDGG(std::size_t hexKey, int resolution) {
      std::size_t key = makeKey();
      const DgHexIDGGS* idggsPtr = hexes[hexKey];
      const DgHexIDGGS& idggs = *idggsPtr;
-     const DgIDGG& idgg = idggs.idgg(7);
+     const DgIDGG& idgg = idggs.idgg(resolution);
      dgIDGGOwner[hexKey].push_back(key);
      dgIDGG[key] = &idgg;
      return key;
 }
+
+const DgIDGG* makeIDGG2(DgHexIDGGS* hex, int resolution) {
+     std::size_t key = makeKey();
+     const DgHexIDGGS* idggsPtr = hex;
+     cout << "hex:" << hex << endl;
+     const DgIDGG* idgg = new DgIDGG(idggsPtr->idgg(resolution));
+     cout << "const DgIDGG& idgg = idggsPtr->idgg(resolution);" << endl;
+     //const DgIDGG* idggPtr =  new DgIDGG(idgg);
+     //cout << "const DgIDGG* idggPtr =  new DgIDGG(idgg);" << endl;
+     dgIDGG[key] = idgg;
+     cout << "idggPtr:" << dgIDGG[key] << endl;
+     return dgIDGG[key];
+}
+
 unsigned long long int nCells(std::size_t idggkey) {
      return dgIDGG[idggkey]->gridStats().nCells();
+}
+unsigned long long int nCells2(DgIDGG* idgg) {
+     cout << "idgg:" << idgg << endl;
+     return idgg->gridStats().nCells();
 }
 long double cls(std::size_t idggkey) {
      return dgIDGG[idggkey]->gridStats().cls();
