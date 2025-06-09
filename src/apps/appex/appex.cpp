2d394223a8f0297beb3e5d4b87fe71c04b0fcbdf
Michael Kmoch <dudelmicha@googlemail.com>
2025-06-09 10:38:22 +0000

    Michael Kmoch - caching and adding parameters

--- src/apps/appex/appex.cpp
+++ src/apps/appex/appex.cpp
@@ -23,111 +23,189 @@
 //
 ////////////////////////////////////////////////////////////////////////////////
 
+#include <cstddef>
 #include <iostream>
 
 using namespace std;
 
-#include <dglib/DgIDGGS4H.h>
 #include "appex.h"
+#include <dglib/DgIDGGS3H.h>
+#include <dglib/DgIDGGS4H.h>
+#include <dglib/DgIDGGS7H.h>
+#include <limits.h>
+#include <map>
+#include <vector>
 ////////////////////////////////////////////////////////////////////////////////
-int doSomething (void)
-{
-   ///// create the DGG /////
+int doSomething(void) {
+  ///// create the DGG /////
 
-   // create the reference frame (RF) conversion network
-   DgRFNetwork net0;
+  // create the reference frame (RF) conversion network
+  DgRFNetwork net0;
 
-   // create the geodetic reference frame
-   // reference frames must be created dynamically using makeRF
-   // they will be deleted by the Network
-   const DgGeoSphRF& geoRF = *(DgGeoSphRF::makeRF(net0, "GS0"));
+  // create the geodetic reference frame
+  // reference frames must be created dynamically using makeRF
+  // they will be deleted by the Network
+  const DgGeoSphRF &geoRF = *(DgGeoSphRF::makeRF(net0, "GS0"));
 
-   // create the ISEA4H grid system with resolutions 0-9; requires a
-   // fixed icosahedron vertex and edge azimuth
-   DgGeoCoord vert0(11.25L, 58.28252559L, false); // args: lon, lat, isRadians
-   long double azimuth = 0.0L;
+  // create the ISEA4H grid system with resolutions 0-9; requires a
+  // fixed icosahedron vertex and edge azimuth
+  DgGeoCoord vert0(11.25L, 58.28252559L, false); // args: lon, lat, isRadians
+  long double azimuth = 0.0L;
 
-   // all DGGS's must be created using a factory makeRF method
-   // the DGGS is memory managed by the DgRFNetwork
-   const DgIDGGS4H* idggsPtr = DgIDGGS4H::makeRF(net0, geoRF, vert0, azimuth, 10);
-   const DgIDGGS4H& idggs = *idggsPtr;
+  // all DGGS's must be created using a factory makeRF method
+  // the DGGS is memory managed by the DgRFNetwork
+  const DgIDGGS4H *idggsPtr =
+      DgIDGGS4H::makeRF(net0, geoRF, vert0, azimuth, 10);
+  const DgIDGGS4H &idggs = *idggsPtr;
 
-   // get the resolution 7 dgg from the dggs
-   const DgIDGG& dgg = idggs.idgg(7);
-   cout << dgg.gridStats() << endl;
+  // get the resolution 7 dgg from the dggs
+  const DgIDGG &dgg = idggs.idgg(7);
+  cout << dgg.gridStats() << endl;
 
-   //////// now use the DGG /////////
+  //////// now use the DGG /////////
 
-   ///// given a point in lon/lat, get the cell it lies within /////
+  ///// given a point in lon/lat, get the cell it lies within /////
 
-   // first create a DgLocation in geoRF coordinates
-   DgGeoCoord geoAddress(-122.7083, 42.1947, false);
-   DgLocation* thePt = geoRF.makeLocation(geoAddress);
-   cout << "the point " << *thePt << endl;
+  // first create a DgLocation in geoRF coordinates
+  DgGeoCoord geoAddress(-122.7083, 42.1947, false);
+  DgLocation *thePt = geoRF.makeLocation(geoAddress);
+  cout << "the point " << *thePt << endl;
 
-   // converting the point location to the dgg RF determines which cell it's in
-   dgg.convert(thePt);
-   cout << "* lies in cell " << *thePt << endl;
+  // converting the point location to the dgg RF determines which cell it's in
+  dgg.convert(thePt);
+  cout << "* lies in cell " << *thePt << endl;
 
-   // we can get the cell's vertices, which are defined in geoRF
-   DgPolygon verts;
-   int ptsPerEdgeDensify = 3;
-   dgg.setVertices(*thePt, verts, ptsPerEdgeDensify);
-   cout << "* with densified cell boundary:\n" << verts << endl;
+  // we can get the cell's vertices, which are defined in geoRF
+  DgPolygon verts;
+  int ptsPerEdgeDensify = 3;
+  dgg.setVertices(*thePt, verts, ptsPerEdgeDensify);
+  cout << "* with densified cell boundary:\n" << verts << endl;
 
-   // we can get the cell's center point by converting the cell back to geoRF
-   geoRF.convert(thePt);
-   cout << "* and cell center point:" << *thePt << endl;
+  // we can get the cell's center point by converting the cell back to geoRF
+  geoRF.convert(thePt);
+  cout << "* and cell center point:" << *thePt << endl;
 
-   // we can extract the coordinates into primitive data types
-   const DgGeoCoord& centCoord = *geoRF.getAddress(*thePt);
-   double latRads = centCoord.lat();
-   double lonRads = centCoord.lon();
-   cout << "* center point lon,lat in radians: "
-        << lonRads << ", " << latRads << endl;
+  // we can extract the coordinates into primitive data types
+  const DgGeoCoord &centCoord = *geoRF.getAddress(*thePt);
+  double latRads = centCoord.lat();
+  double lonRads = centCoord.lon();
+  cout << "* center point lon,lat in radians: " << lonRads << ", " << latRads
+       << endl;
 
-   const DgGeoCoord& firstVert = *geoRF.getAddress(verts[0]);
-   double latDegs = firstVert.latDegs();
-   double lonDegs = firstVert.lonDegs();
-   cout << "* first boundary vertex lon,lat in degrees: "
-        << lonDegs << ", " << latDegs << endl;
+  const DgGeoCoord &firstVert = *geoRF.getAddress(verts[0]);
+  double latDegs = firstVert.latDegs();
+  double lonDegs = firstVert.lonDegs();
+  cout << "* first boundary vertex lon,lat in degrees: " << lonDegs << ", "
+       << latDegs << endl;
 
-   delete thePt;
+  delete thePt;
 
-   return 0;
-
+  return 0;
 }
+std::size_t current_key = 0;
+std::map<std::size_t, DgRFNetwork> networks;
+std::map<std::size_t, const DgHexIDGGS *> hexes;
+std::map<std::size_t, const DgGeoSphRF *> geoSpheres;
+std::map<std::size_t, const DgIDGG*> dgIDGG;
+std::map<std::size_t, vector<std::size_t>> hexesOwner;
+std::map<std::size_t, vector<std::size_t>> geoSpheresOwner;
+std::map<std::size_t, vector<std::size_t>> dgIDGGOwner;
 
 extern "C" {
+     
+enum Resoulution { ISEA3H = 1, ISEA4H = 2, ISEA7H = 3};
+std::size_t makeKey() {
+  std::size_t newkey;
+  current_key = current_key + 1;
+  newkey = current_key;
+  return newkey;
+}
+std::size_t makeNetwork() {
+  std::size_t key = makeKey();
+  networks[key] = DgRFNetwork();
+  return key;
+}
+void destroyNetwork(std::size_t key) {
+  networks.erase(key);
+  for (const int &i : hexesOwner[key]) {
+    for (const int &j : dgIDGGOwner[i])
+      dgIDGG.erase(j);
+    hexes.erase(i);
+  }
+  for (const int &i : geoSpheresOwner[key])
+    geoSpheres.erase(i);
+}
+std::size_t makeGeoSphere(std::size_t networkKey, char* type) {
+  std::size_t key = makeKey();
+  geoSpheres[key] = DgGeoSphRF::makeRF(networks[networkKey], type);
+  geoSpheresOwner[networkKey].push_back(key);
+  return key;
+}
+std::size_t makeHex(std::size_t networkKey, std::size_t geoSphereKey,
+                    Resoulution resolution, long double lon, long double lat,
+                    bool rads, long double azimuth, int res) {
+  std::size_t key = makeKey();
+  switch (resolution) {
+  case ISEA3H:
+    hexes[key] =
+        DgIDGGS3H::makeRF(networks[networkKey], *geoSpheres[geoSphereKey],
+                          DgGeoCoord(lon, lat, rads), azimuth, res);
+    break;
+  case ISEA4H:
+    hexes[key] =
+        DgIDGGS4H::makeRF(networks[networkKey], *geoSpheres[geoSphereKey],
+                          DgGeoCoord(lon, lat, rads), azimuth, res);
+    break;
+  case ISEA7H:
+    hexes[key] =
+        DgIDGGS7H::makeRF(networks[networkKey], *geoSpheres[geoSphereKey],
+                          DgGeoCoord(lon, lat, rads), azimuth, res);
+    break;
+  }
+  hexesOwner[networkKey].push_back(key);
+  return key;
+}
+std::size_t makeIDGG(std::size_t hexKey, int resolution) {
+     std::size_t key = makeKey();
+     const DgHexIDGGS* idggsPtr = hexes[hexKey];
+     const DgHexIDGGS& idggs = *idggsPtr;
+     const DgIDGG& idgg = idggs.idgg(7);
+     dgIDGGOwner[hexKey].push_back(key);
+     dgIDGG[key] = &idgg;
+     return key;
+}
+unsigned long long int nCells(std::size_t idggkey) {
+     return dgIDGG[idggkey]->gridStats().nCells();
+}
+long double cls(std::size_t idggkey) {
+     return dgIDGG[idggkey]->gridStats().cls();
+}
+long double cellAreaKM(std::size_t idggkey) {
+     return dgIDGG[idggkey]->gridStats().cellAreaKM();
+}
+unsigned long long int doSomething2(void) {
+  ///// create the DGG /////
 
-     unsigned long long int  doSomething2 (void)
-     {
-     ///// create the DGG /////
+  // create the reference frame (RF) conversion network
+  size_t net0Key = makeNetwork();
 
-     // create the reference frame (RF) conversion network
-     DgRFNetwork net0;
+  // create the geodetic reference frame
+  // reference frames must be created dynamically using makeRF
+  // they will be deleted by the Network
+  size_t geoSphereKey = makeGeoSphere(net0Key, "GS0");
 
-     // create the geodetic reference frame
-     // reference frames must be created dynamically using makeRF
-     // they will be deleted by the Network
-     const DgGeoSphRF& geoRF = *(DgGeoSphRF::makeRF(net0, "GS0"));
+  // all DGGS's must be created using a factory makeRF method
+  // the DGGS is memory managed by the DgRFNetwork
+  size_t hexKey = makeHex(net0Key, geoSphereKey, ISEA4H, 11.25L, 58.28252559L,
+                          false, 0, 10);
 
-     // create the ISEA4H grid system with resolutions 0-9; requires a
-     // fixed icosahedron vertex and edge azimuth
-     DgGeoCoord vert0(11.25L, 58.28252559L, false); // args: lon, lat, isRadians
-     long double azimuth = 0.0L;
+  // get the resolution 7 dgg from the dggs
+  size_t idggkey = makeIDGG(hexKey,7);
+  //const DgIDGG &dgg = dgIDGG[idggkey];
+  // cout << dgg.gridStats() << endl;
 
-     // all DGGS's must be created using a factory makeRF method
-     // the DGGS is memory managed by the DgRFNetwork
-     const DgIDGGS4H* idggsPtr = DgIDGGS4H::makeRF(net0, geoRF, vert0, azimuth, 10);
-     const DgIDGGS4H& idggs = *idggsPtr;
-
-     // get the resolution 7 dgg from the dggs
-     const DgIDGG& dgg = idggs.idgg(7);
-     // cout << dgg.gridStats() << endl;
-
-     return dgg.gridStats().nCells();
-     }
-} 
+  return nCells(idggkey);
+}
+}
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
